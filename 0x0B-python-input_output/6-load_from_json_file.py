9efa957803bf6ea6d067b2b00b68983bb63a5952
AhmedSherif <ahmed_sherif619@yahoo.cm>
2024-01-10 18:13:59 -0800

    IO v3

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,5 +7,5 @@ import json
 
 def load_from_json_file(filename):
     """creates object from JSON representation in file"""
-    with open(filename, "r", encoding = "UTF-8") as f :
+    with open(filename, "r", encoding="UTF-8") as f:
         return json.load(f)
