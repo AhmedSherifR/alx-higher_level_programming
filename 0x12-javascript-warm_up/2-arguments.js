38594b778536ddc230be41a5ac3bb49665c7852b
AhmedSherif <ahmed_sherif619@yahoo.cm>
2024-03-14 13:07:49 -0700

    js warm up

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Prints a message depending on number of arguments passed
+
+const args = process.argv.length;
+if (args === 2){
+	console.log('No argument');
+} else if (args === 3){
+	console.log('Argument found');
+} else {
+	console.log('Arguments found');
+}
